2f37c23ec5162aeb9cffeb930feaff2b781ad08c
ZhukovAlexander <zhukovaa90@gmail.com>
2016-04-08 15:30:08 +0000

    Add call_context method

--- lambdify/decorators.py
+++ lambdify/decorators.py
@@ -1,5 +1,6 @@
 import json
 import functools
+from contextlib import contextmanager
 
 import boto3
 import botocore
@@ -8,61 +9,6 @@ import dill
 from deployment import DeploymentPackage
 
 dill.settings['recurse'] = True
-
-
-class LambdaProxy(object):
-    def __init__(self, lambda_instance, client):
-        self.client = client
-        self.lambda_instance = lambda_instance
-
-    def create(self):
-        response = self.client.create_function(
-                FunctionName=self.lambda_instance.name,
-                Runtime='python2.7',
-                Role=self.lambda_instance.role,
-                Handler='container.lambda_handler',
-                Code={
-                    'ZipFile': self.lambda_instance.package.to_bytes(self.lambda_instance.dumped_code),
-                    # 'S3Bucket': self.bucket,
-                    # 'S3Key': self.key,
-                    # 'S3ObjectVersion': 'string'
-                },
-                Description=self.lambda_instance.description,
-                Timeout=123,
-                MemorySize=128,
-                Publish=True,
-
-        )
-
-        return response
-
-    def get(self):
-        return self.client.get_function(FunctionName=self.lambda_instance.name)
-
-    def update(self):
-        response = self.client.update_function_code(
-                FunctionName=self.lambda_instance.name,
-                ZipFile=self.lambda_instance.package.zip_bytes(self.lambda_instance.dumped_code),
-                # S3Bucket='string',
-                # S3Key='string',
-                # S3ObjectVersion='string',
-                # Publish=True|False
-        )
-
-        return response
-
-    def invoke(self, event, context, inv_type='RequestResponse', log_type='None', version=None):
-        params = dict(
-                FunctionName=self.lambda_instance.name,
-                InvocationType=inv_type,
-                LogType=log_type,
-                # ClientContext='string',
-                Payload=json.dumps(event),
-        )
-        if version:
-            params['Qualifier'] = version
-
-        return self.client.invoke(**params)
 
 
 # this flags allow you to control the point in time at which to create/update your function
@@ -80,25 +26,19 @@ class Lambda(object):
     This wrapper basically replaces the original function with it's AWS Lambda instance.
     When called, the instance of this class will route the call to the AWS Instance, instead of
     calling a local function.
-    
 
 
     """
     _was_updated = False
 
+    _context = None
+    _inv_type = 'RequestResponse'
+
+    version = '$LATEST'
+
     def __init__(self, func, name='', role='', description='', vps_config=None, package=None, flags=UPDATE_EXPLICIT):
         """
         Main Lambda constructor
-
-        >>>from lambdify import Lambda
-        >>>
-        >>>def echo(event):
-        ...    return event
-        >>>
-        >>>echo = Lambda(echo, name='echo')
-        >>>echo.create()
-
-
 
         :param func: function to make an AWS Lambda from. This will be the actual lambda handler
         :param name:
@@ -108,6 +48,16 @@ class Lambda(object):
         :param package: deployment package for this function
         :param flags: this flags allow you to control the point in time, when to make an actual call to aws to create
         the function
+
+        Usage:
+
+        >>>from lambdify import Lambda
+        >>>
+        >>>func = Lambda(lambda x: x)
+        >>>func.name
+        '__main__.<lambda>'
+        >>>func.create()
+        {...}
         """
         self.client = boto3.client('lambda', region_name='us-west-2')
 
@@ -122,6 +72,7 @@ class Lambda(object):
         self.vps_config = vps_config or {}
         self.package = package or DeploymentPackage(self)
         self.create_options = flags
+        self._context = {}
 
         # here we need to adapt the signature of the function to the AWS Lambda signature
         # according to https://docs.aws.amazon.com/lambda/latest/dg/python-programming-model-handler-types.html
@@ -137,8 +88,6 @@ class Lambda(object):
         # by this function
         self.dumped_code = dill.dumps(self.functor)
 
-        self.proxy = LambdaProxy(self, self.client)
-
         if self.create_options & UPDATE_ON_INIT == UPDATE_ON_INIT:
             self._create_or_update()
 
@@ -146,13 +95,6 @@ class Lambda(object):
     def f(cls, name='', role='', description='', vps_config=None, package=None, flags=UPDATE_EXPLICIT):
         """
         Alternative constructor factory to allow this class to be used as a decorator
-
-        >>>from lambdify import Lambda
-        >>>@Lambda.f(name='echo')
-        ...def echo(event):
-        ...    return event
-        ...
-        >>>echo.create()
 
 
         :param name: lambda function name
@@ -173,17 +115,40 @@ class Lambda(object):
                        vps_config=vps_config,
                        package=package,
                        flags=flags)
-
         return initialize
 
-    def __call__(self, *args, **kwargs):
-        """Proxies calls to the wrapped callable instance to the actual cloud lambda.
-        Note the signature of this method allows you to call it as a casual python function.
-        Arguments will be automatically adapted to the (event, context) pair of argument, as required for
-        lambda handler.
+    @contextmanager
+    def call_context(self, version=None, inv_type=None, context=None):
+        """Context managers, that allows the Lambda to be called with a
+        specific version, context and invocation type
+
+        :param  version: lambda function will use this version
+        :param inv_type: invocation type
+        :param context: lambda context
+
+        >>>l = Lambda(lambda x: x, name='foo')
+        >>>
+        >>>with l.call_context(version='42', context={'bar': 'bar'}, inv_type='Event'):
+        ...print l.version, l._context, l._inv_type
+        ('42', {'bar': 'bar'}, 'Event')
+        >>>
+        >>>print l.version, l._context, l._inv_type
+        ('$LATEST', {}, 'RequestResponse')
+
         """
+        _version_orig, _inv_type_orig, _context_orig = self.version, self._inv_type, self._context.copy()
+        try:
+            self.version = version or self.version
+            self._inv_type = inv_type or self._inv_type
+            self._context = context or self._context
+            yield
+        finally:
+            self.version, self._inv_type, self._context = _version_orig, _inv_type_orig, _context_orig
+
+    def __call__(self, *args, **kwargs):
         kwargs.update({'args': args})
-        return json.loads(self.proxy.invoke(kwargs, None)['Payload'].read())
+        resp = self.invoke(kwargs, self._context, version=self.version, inv_type=self._inv_type)
+        return json.loads(resp['Payload'].read())
 
     def _create_or_update(self):
         try:
@@ -199,25 +164,59 @@ class Lambda(object):
 
     def create(self):
         """Create lambda function in AWS"""
-        return self.proxy.create()
+        response = self.client.create_function(
+                FunctionName=self.name,
+                Runtime='python2.7',
+                Role=self.role,
+                Handler='container.lambda_handler',
+                Code={
+                    'ZipFile': self.package.zip_bytes(self.dumped_code),
+
+                },
+                Description=self.description,
+                Timeout=123,
+                MemorySize=128,
+                Publish=True,
+
+        )
+
+        return response
 
     def get(self):
         """Get the lambda instance details from AWS"""
-        return self.proxy.get()
+        return self.client.get_function(FunctionName=self.name)
 
     def update(self):
         """Update the lambda instance"""
-        return self.proxy.update()
+        response = self.client.update_function_code(
+                FunctionName=self.name,
+                ZipFile=self.package.zip_bytes(self.dumped_code),
+                # Publish=True|False
+        )
 
-    def invoke(self, event, context):
-        """Invoke the lambda function.
+        return response
+
+    def invoke(self, event, context, inv_type=None, log_type='None', version=None):
+        """Invoke the lambda function This is basically a low-level lambda interface.
+        In most cases, you won't need to use this by yourself.
+
+        :param event: lambda input
+        :param context: lambda execution client context
+        :param inv_type: invocation type
+        :param log_type: log type
+        :param version: version
         """
         if not self._was_updated and self.create_options & UPDATE_LAZY == UPDATE_LAZY:
             self._create_or_update()
-        return self.proxy.invoke(event, context)
 
-    def _serialize(self):
-        return dill.dumps(self.functor)
+        params = dict(
+                FunctionName=self.name,
+                InvocationType=inv_type or self._inv_type,
+                LogType=log_type,
+                ClientContext=json.dumps(context),
+                Payload=json.dumps(event),
+        )
+        if version:
+            params['Qualifier'] = version
 
-    def _serialize_to(self, f):
-        return dill.dump(self.functor, f)
+        return self.client.invoke(**params)
